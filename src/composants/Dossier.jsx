fa0fe60023b1257a9076ff9cb985397b29ba4ff8
Abdirahman Omar <abdirahmano217@gmail.com>
2021-03-01 00:20:30 -0500

    Deuxieme dépot

--- src/composants/Dossier.jsx
+++ src/composants/Dossier.jsx
@@ -1,15 +1,20 @@
+import "./ListeDossiers"
+import "./Dossier.scss"
+import { MdSort } from 'react-icons/md';
+import MoreVertTwoToneIcon from '@material-ui/icons/MoreVertTwoTone';
+
 export default function Dossier({id, titre, couleur, dateModif}) {
   return (
-    <article className="Dossier">
-      <div className="couverture">
-        <span className="deplacer">Déplacer</span>
+    <article className="Dossier" style={{backgroundColor:couleur}}>
+      <div className="couverture" >
+        <span className="deplacer"><MdSort size={50}/></span>
         <img src={`images-dossiers/${id}.png`} alt={titre}/>
       </div>
       <div className="info">
         <h2>{titre}</h2>
-        <p>{dateModif}</p>
+        <p>Modifié: {dateModif}</p>
       </div>
-      <button>Modifier</button>
+      <button><MoreVertTwoToneIcon/></button>
     </article>
   );
 }
